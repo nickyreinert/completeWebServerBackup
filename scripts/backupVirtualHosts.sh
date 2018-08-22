012543773d4ee0bbfd9d9806eab262886c8663ab
nickyreinert <mail@nickyreinert.de>
2018-08-22 11:30:21 +0200

    Update and rename backupVirtualHosts.sh to scripts/backupVirtualHosts.sh
    moved to scripts-folder
    added documentation

--- scripts/backupVirtualHosts.sh
+++ scripts/backupVirtualHosts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # get to current script base path
-# thanks to https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 BASE_PATH_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $BASE_PATH_SCRIPT
 
@@ -12,23 +11,29 @@ source backup.conf
 configFilesString=$(/usr/sbin/apache2ctl -S | grep "port 80 namevhost" | awk -F ' ' '{ print $5 }' | sed -E 's/[:()]//g' | sed -E 's/[ 0-9]$//g')
 
 # explode this string to an array that can be looped through
-# thanks to Dennis Williamson @ https://stackoverflow.com/a/31405855/2360229
 configFiles=($(echo "$configFilesString" | tr ',' ' '))
 
 # loop through every configfile
 for configFile in "${configFiles[@]}"
 do
 
-
+        # if current config file is a file or a symbolic link, use it
         if [[ -f "$configFile" || -L "$configFile" ]]
         then
 
+                # grep / search the config file for the location of the current virtual host's document root
+                # thats the actual folder we want to backup 
+                # put this into a string
                 srcFolder=$(grep -oE 'DocumentRoot \"(.*)\"' $configFile | awk -F ' ' '{ print $2 }' | sed -E 's/["]//g')
 
+                # if the current string has a length, provide this information to the actual backup script
                 len=${#srcFolder}
-
                 if [ "$len" -gt "0" ]
                 then
+                        # we also provide a destination folder, which is just the last part of the 
+                        # document root, because we do not want to create the backup in e.g.
+                        # /backup-location/var/www/sample/sub-domain but in
+                        # /backup-location/sub-domain
                         ./backupFilesystem.sh -f $srcFolder -d "${BASE_PATH_BACKUP}htdocs/$(basename $srcFolder)"
                 fi
 
